2439765d4b7564b1a2b58dda4025c3ca335f3e81
om_lf_ur <om.ur.xkk87@gmail.com>
2019-01-20 14:53:57 +0900

    initial commit : hello world

---
+++ symfony4/src/Controller/RootController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RootController extends AbstractController
+{
+    /**
+     * @Route("/root", name="root")
+     */
+    public function index()
+    {
+        return $this->render('root/index.html.twig', [
+            'controller_name' => 'RootController',
+        ]);
+    }
+}
